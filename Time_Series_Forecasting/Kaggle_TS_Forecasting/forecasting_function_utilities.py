68ae59279c9d1412866d8d19bdaf1eb512b4d1f2
MarioRasconMerinoML <105947574+MarioRasconMerinoML@users.noreply.github.com>
2023-10-22 16:40:33 +0200

    Update forecasting_function_utilities.py

--- Time_Series_Forecasting/Kaggle_TS_Forecasting/forecasting_function_utilities.py
+++ Time_Series_Forecasting/Kaggle_TS_Forecasting/forecasting_function_utilities.py
@@ -1,4 +1,55 @@
+class BoostedHybrid:
+    '''
+        Create LinearRegression + XGBRegressor hybrid with BoostedHybrid
+        model = BoostedHybrid(model_1 = LinearRegression(fit_intercept=False), model_2 = XGBRegressor())
 
+        Fit and predict
+        model.fit(X_1, X_2, y)
+        y_pred = model.predict(X_1,X_2)
+
+        y_pred = y_pred.clip(0.0)
+    '''
+    def __init__(self, model_1, model_2):
+        self.model_1 = model_1
+        self.model_2 = model_2
+        self.y_columns = None  # store column names from fit method
+        
+    def fit(self, X_1, X_2, y):
+        # Fit self.model_1
+        self.model_1.fit(X_1, y)
+        y_fit = pd.DataFrame(
+            # Make predictions with self.model_1
+            self.model_1.predict(X_1),
+            index=X_1.index, 
+            columns=y.columns,
+        )
+
+        # Compute residuals
+        y_resid = y - y_fit
+        y_resid = y_resid.stack().squeeze() # wide to long
+
+        # Fit self.model_2 on residuals
+        self.model_2.fit(X_2, y_resid)
+
+        # Save column names for predict method
+        self.y_columns = y.columns
+        # Save data for question checking
+        self.y_fit = y_fit
+        self.y_resid = y_resid
+    
+    def predict(self, X_1, X_2):
+        y_pred = pd.DataFrame(
+            # Predict with self.model_1
+            self.model_1.predict(X_1),
+            index=X_1.index, columns=self.y_columns,
+        )
+        y_pred = y_pred.stack().squeeze()  # wide to long
+
+        # add self.model_2 predictions to y_pred
+        y_pred += self.model_2.predict(X_2)
+        
+        return y_pred.unstack()  # long to wide
+        
 def plot_multistep(y, every=1, ax=None, palette_kwargs=None):
     palette_kwargs_ = dict(palette='husl', n_colors=16, desat=None)
     if palette_kwargs is not None:
